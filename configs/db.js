e077fdd41ed751874b32b89f0f4c4f8d506c8642
kajol201 <mdraselislamkajol201@gmail.com>
2025-11-01 23:03:25 +0600

    nice

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/Department-portal`);
+        const conn = await mongoose.connect(`${process.env.MONGODB_URL}/Department-portal`);
         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error("❌ MongoDB connection failed:", error.message);
