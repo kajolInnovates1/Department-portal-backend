34141eb77ccff1c5dcb265fc05b8b7f86e89795f
kajol201 <mdraselislamkajol201@gmail.com>
2025-11-01 19:28:06 +0600

    last

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
+// server.js
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
-import { inngest, functions } from './inngest/index.js'
+import { inngest, functions } from './inngest/index.js';
 import { serve } from 'inngest/express';
+
 const app = express();
 
+// Connect to MongoDB
 await connectDB();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => res.send('server is Running'))
-app.use('/api/inngest', serve({ client: inngest, functions }))
+// Test route
+app.get('/', (req, res) => res.send('Server is running'));
 
+// Inngest routes
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server is Running on port${PORT}`))
-
+// Export the app instead of listening
+export default app;
